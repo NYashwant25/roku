d86814548a09e2f8a2e15fc9481eaad95b2e07da
N Yashwant <nyashwant25@gmail.com>
2021-06-24 11:17:47 +0530

    make it better

--- src/orm/orm.config.ts
+++ src/orm/orm.config.ts
@@ -4,7 +4,7 @@ import { UserEntity } from 'src/user/user.entity';
 export const config: TypeOrmModuleOptions = {
     type: 'postgres',
     host: 'localhost',
-    port: 5432,
+    port: 3000,
     username: 'postgres',
     password: '1922.comj',
     database: 'roku_db',
